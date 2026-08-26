d716eff56af6484f787177e42c6e9c2c1ed9af68
Huiz-LD <17283028@qq.com>
2026-08-26 21:28:11 +0800

    runtime 可以启动 读取 json 文件正常, 后续处理逻辑待验证, 感觉代码比较.考虑重构...

--- core/src/deploy-to-3562.sh
+++ core/src/deploy-to-3562.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-SRC="${SCRIPT_DIR}/drivers/plugins/native/can"
+SRC="${SCRIPT_DIR}/drivers/plugins/native/canopen"
 TARGET="root@192.168.100.100:/opt/openplc-runtime/core/src/drivers/plugins/native/"
 
 echo "Deploying ${SRC} to ${TARGET}..."
